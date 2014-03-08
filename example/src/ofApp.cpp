eeda8d97accdeb04d68998c092bdf06b6359b164
Christopher Baker <me@christopherbaker.net>
2014-03-07 23:23:01 -0600

    More example cleanup.

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -50,8 +50,8 @@ void ofApp::setup()
 		.use("runid",last_run_id)
 		.use("gdata", "MyData")
         .execute();
-        cout << "insert into game_run_data error:" << sqlite.getError() << endl;;
 
+        cout << "insert into game_run_data error: " << sqlite.getError() << endl;
 
         // insert
         sqlite.insert("scores")
@@ -68,12 +68,12 @@ void ofApp::setup()
         // get last inserted row id
         cout << "inserted row id: " << sqlite.lastInsertID() << endl;
 
-
         ofxSQLiteSelect sel = sqlite.select("id, time").from("scores");
 
         sel.execute().begin();
 
-        while(sel.hasNext()) {
+        while(sel.hasNext())
+        {
             int id = sel.getInt();
             std::string name = sel.getString();
             cout << id << ", " << name << endl;
@@ -128,33 +128,33 @@ void ofApp::setup()
         {
             cout << "ERROR CREATE TABLE\n";
         }
-//
-//        // just pasted this example here from a project I did.. (did no test it,
-//        // but shows you some things you can do with sqlite tables
-//        // ------------------------------------------------------------------------
-//        int r = sqlite->simpleQuery("CREATE TABLE IF NOT EXISTS tweets (" \
-//                                    " id INTEGER PRIMARY KEY AUTOINCREMENT " \
-//                                    ",avatar VARCHAR(255) " \
-//                                    ",user_id VARCHAR(100) " \
-//                                    ",screen_name VARCHAR(50)" \
-//                                    ",tweet_id VARCHAR(50) " \
-//                                    ",date_created DATETIME DEFAULT CURRENT_TIMESTAMP "\
-//                                    ",date_exported DATETIME "\
-//                                    ",contains_face BOOLEAN " \
-//                                    ",is_used BOOLEAN " \
-//                                    ",is_fetched BOOLEAN " \
-//                                    ",is_exported BOOLEAN " \
-//                                    ");"
-//                                    );
-//
+
+        // just pasted this example here from a project I did.. (did no test it,
+        // but shows you some things you can do with sqlite tables
+        // ------------------------------------------------------------------------
+        int r = sqlite.simpleQuery("CREATE TABLE IF NOT EXISTS tweets (" \
+                                    " id INTEGER PRIMARY KEY AUTOINCREMENT " \
+                                    ",avatar VARCHAR(255) " \
+                                    ",user_id VARCHAR(100) " \
+                                    ",screen_name VARCHAR(50)" \
+                                    ",tweet_id VARCHAR(50) " \
+                                    ",date_created DATETIME DEFAULT CURRENT_TIMESTAMP "\
+                                    ",date_exported DATETIME "\
+                                    ",contains_face BOOLEAN " \
+                                    ",is_used BOOLEAN " \
+                                    ",is_fetched BOOLEAN " \
+                                    ",is_exported BOOLEAN " \
+                                    ");"
+                                    );
+
 //        // inserting mass amount of entries: use transations
 //        // -------------------------------------------------------------------------
-//        if (SQLITE_OK != sqlite->simpleQuery("BEGIN TRANSACTION;")) {
+//        if (SQLITE_OK != sqlite.simpleQuery("BEGIN TRANSACTION;")) {
 //            cout << "ERROR: cannot begin transaction" << std::endl;
 //        }
 //        
 //        for(int i = 0; i < num_entries; ++i) {
-//            int result = sqlite->insert("photos")
+//            int result = sqlite.insert("photos")
 //			.use("old_name", "old_name")
 //			.use("new_name","new_name")
 //			.use("dir_name", "IMAGE2008.08.06/")
@@ -163,7 +163,7 @@ void ofApp::setup()
 //            .execute();
 //            if(result != SQLITE_OK) {
 //                cout << "error: " << result << endl;
-//                cout << "message:" << sqlite->getError() << endl;
+//                cout << "message:" << sqlite.getError() << endl;
 //                break;
 //            }
 //            else {
@@ -171,45 +171,45 @@ void ofApp::setup()
 //            }
 //            
 //        }
-//    
+
 //        cout << "Inserted: " << ok << std::endl;
-//        if (SQLITE_OK != sqlite->simpleQuery("COMMIT;")) {
+//        if (SQLITE_OK != sqlite.simpleQuery("COMMIT;")) {
 //            cout << "ERROR: cannot commit" << std::endl;
 //        }
 //        
 //        // Get amount of rows.
 //        //-------------------------------------------------------------------------
-//        ofxSQLiteSelect sel = sqlite->select("count(id) as total").from("photos");
+//        ofxSQLiteSelect sel = sqlite.select("count(id) as total").from("photos");
 //        sel.execute().begin();
 //        int count = sel.getInt();
 //        cout << "total entries:" << count << std::endl;
-//        
-//        // Example of using a timestamp (not tested in this code; purely as example)
-//        // -------------------------------------------------------------------------
-//        /*
-//         // table with a couple of date fields
-//         db = new ofxSQLite(ofToDataPath("tweets.db"));
-//         int r = db->simpleQuery("CREATE TABLE IF NOT EXISTS tweets (" \
-//         " id INTEGER PRIMARY KEY AUTOINCREMENT " \
-//         ",avatar VARCHAR(255) " \
-//         ",user_id VARCHAR(100) " \
-//         ",screen_name VARCHAR(50)" \
-//         ",tweet_id VARCHAR(50) " \
-//         ",date_created DATETIME DEFAULT CURRENT_TIMESTAMP "\
-//         ",date_exported DATETIME "\
-//         ",contains_face BOOLEAN " \
-//         ",is_used BOOLEAN " \
-//         ",is_fetched BOOLEAN " \
-//         ",is_exported BOOLEAN " \
-//         ");"
-//         );
-//         
-//         // Updating the time field with a current timestamp:
-//         // you can use "now()" to get a timestamp object
-//         int r = db->update("tweets").use("is_exported",true).use("date_exported",db->now()).execute();
-//         if(r != SQLITE_OK) {
-//         ofLog(OF_LOG_ERROR, "error: cannot set export to true");
-//         }
-//         */
+
+        // Example of using a timestamp (not tested in this code; purely as example)
+        // -------------------------------------------------------------------------
+        /*
+         // table with a couple of date fields
+         db = new ofxSQLite(ofToDataPath("tweets.db"));
+         int r = db->simpleQuery("CREATE TABLE IF NOT EXISTS tweets (" \
+         " id INTEGER PRIMARY KEY AUTOINCREMENT " \
+         ",avatar VARCHAR(255) " \
+         ",user_id VARCHAR(100) " \
+         ",screen_name VARCHAR(50)" \
+         ",tweet_id VARCHAR(50) " \
+         ",date_created DATETIME DEFAULT CURRENT_TIMESTAMP "\
+         ",date_exported DATETIME "\
+         ",contains_face BOOLEAN " \
+         ",is_used BOOLEAN " \
+         ",is_fetched BOOLEAN " \
+         ",is_exported BOOLEAN " \
+         ");"
+         );
+         
+         // Updating the time field with a current timestamp:
+         // you can use "now()" to get a timestamp object
+         int r = db->update("tweets").use("is_exported",true).use("date_exported",db->now()).execute();
+         if(r != SQLITE_OK) {
+         ofLog(OF_LOG_ERROR, "error: cannot set export to true");
+         }
+         */
 
 }
